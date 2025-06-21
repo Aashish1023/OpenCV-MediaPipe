5299273783e7ce9226241973f78db6b27009fc26
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-06-21 21:12:58 +0545

    Add initial implementation of volume control script with necessary imports

---
+++ volume control.py
@@ -0,0 +1,12 @@
+#step num 1 import libraries
+
+import cv2  // OpenCV for image processing
+import numpy as np // NumPy for numerical operations
+import mediapipe as mp  //detect hands
+
+#step num 2 initialize mediapipe hands
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import CLSCTX_ALL  // For controlling audio volume
+import math  // For mathematical operations
+
+
